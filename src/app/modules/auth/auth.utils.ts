8163f8d3a60cd2732d3a91f2ffcc95f412c4e3ef
IshtyMahin <ishty119@gmail.com>
2025-04-19 17:19:25 +0600

    some update in auth

--- src/app/modules/auth/auth.utils.ts
+++ src/app/modules/auth/auth.utils.ts
@@ -1,22 +1,15 @@
+// @ts-nocheck
 import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
 import { IJwtPayload } from './auth.interface';
-import { SignOptions } from 'jsonwebtoken';
 
 export const createToken = (
-  jwtPayload: IJwtPayload,
-  secret: Secret,
-  expiresIn: string | number,
+    jwtPayload: IJwtPayload,
+    secret: jwt.Secret,
+    expiresIn: string | number,
 ) => {
-  const signOptions: SignOptions = {
-    expiresIn: expiresIn as SignOptions['expiresIn'], 
-  };
-
-  return jwt.sign(jwtPayload, secret, signOptions);
+    return jwt.sign(jwtPayload, secret, { expiresIn });
 };
 
-
 export const verifyToken = (token: string, secret: Secret) => {
-    
-    
     return jwt.verify(token, secret) as JwtPayload;
 };
